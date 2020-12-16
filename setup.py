cf35177440615a77336b1f1d65f37882f9ad5649
Deepanshu Karnwal <deepanshu.k@hcl.com>
2020-12-16 05:31:57 +0000

    Revert "[Issue-Id:RICAPP-155]Code changes to support for JJB and Unit testing for AD xApp"
    This reverts commit f98ee76af036d60b8f5077105830ed61a13ed5aa.
    
    Reason for revert: <will commit these changes in cherry release maintenance release in Jan>
    
    Change-Id: Ifcbf3bb392e4020ad21a279d3a59ee91971413b3
    Signed-off-by: deepanshuk <deepanshu.k@hcl.com>

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ setup(
     packages=find_packages(exclude=["tests.*", "tests"]),
     description="Anomaly Detection xApp that integrates with Traffic Steering",
     url="https://gerrit.o-ran-sc.org/r/admin/repos/ric-app/ad",
-    install_requires=["ricxappframe>=1.1.1,<2.0.0", "pandas>=1.1.3", "joblib>=0.3.2", "Scikit-learn>=0.21", "schedule>=0.0.0", "hdbscan>=0.8.13"],
-    entry_points={"console_scripts": ["run-ad.py=ad.main:start"]},  # adds a magical entrypoint for Docker
+    #install_requires=["ricxappframe>=1.1.1,<2.0.0"],
+    #entry_points={"console_scripts": ["run-ad.py=ad.main"]},  # adds a magical entrypoint for Docker
     license="Apache 2.0",
     data_files=[("", ["LICENSE.txt"])],
 )
